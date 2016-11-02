aeec2180006c12eef44744c9c1e93445bde8bed3
acyc <acyctech@163.com>
2016-11-02 15:50:42 +0800

    seconde commit
    seconde commit

---
+++ Saluton.java
@@ -0,0 +1,8 @@
+//give this program a name:Saluton
+public class Saluton{
+	//set a entrance for this program
+	public static void main(String[] args){
+		//output information
+		System.out.println("Saluton mondo!");
+	}
+}
